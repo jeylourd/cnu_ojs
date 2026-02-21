0eadd41614072d256aa84390533250445dfa3528
jLurd <jeylourdalcontin@gmail.com>
2026-02-21 15:18:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { GlobalPreloader } from "@/components/ui/GlobalPreloader";
+import dynamic from "next/dynamic";
+
+const GlobalPreloader = dynamic(() => import("@/components/ui/GlobalPreloader"), { ssr: false });
 import "./globals.css";
 
 const geistSans = Geist({
